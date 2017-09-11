c92616618f8e15bab399d48d65dbecea45e593e0
Boyer <nboyer>
2017-09-11 13:54:40 -0500

    Half way through completion of adding tracking ability of all K-State Aircraft on map.

--- WeatherRadar/PlaneLocations.cs
+++ WeatherRadar/PlaneLocations.cs
@@ -5,7 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
 using System.Net;
+using System.Data;
+using System.Xml;
 using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 namespace WeatherRadar
 {
@@ -19,14 +22,37 @@ namespace WeatherRadar
 
         public void getPlaneLocations()
         {
+            string flightLoc;
             using (WebClient client = new WebClient())
             {
-                var flightLoc = client.DownloadString("http://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?lat=38.79797&lng=-97.64008&fDstL=0&fDstU=100");
-                JObject json = JObject.Parse(flightLoc);
-                Debug.WriteLine(json);
+                flightLoc = client.DownloadString("http://public-api.adsbexchange.com/VirtualRadar/AircraftList.json?lat=38.79797&lng=-97.64008&fDstL=0&fDstU=100");
+                JObject flightJson = JObject.Parse(flightLoc);
             }
 
-        }
+            XmlDocument flightXml = (XmlDocument)JsonConvert.DeserializeXmlNode(flightLoc, "acList");
+
+            DataSet flightDataSet = new DataSet();
+            flightDataSet.ReadXml(new XmlNodeReader(flightXml));
+            //otherdataset.ReadXml(new StringReader(rawAlertString));
+            DataTable flightTable = new DataTable();
+            flightTable = flightDataSet.Tables[0];
+            Debug.WriteLine(flightTable);
+
+            foreach (DataRow row in flightTable.Rows)
+            {
+                Debug.WriteLine(row["Call"]);
+                /*
+                if (row["category"].ToString() == "Met")
+                {
+                    if (row["polygon"].ToString() != "")
+                    {
+
+                    }
+                }
+                */
+
+            }
+        }          
 
     }
 
